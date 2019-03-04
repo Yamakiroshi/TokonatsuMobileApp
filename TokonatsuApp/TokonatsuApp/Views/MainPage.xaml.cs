55b36f870eaa2c6d9b53d877893c9ac822b905f6
Adam John Hay <adam.hay@glazingvision.co.uk>
2019-03-04 17:06:49 +0000

    Updates to the application (Android)

--- TokonatsuApp/TokonatsuApp/Views/MainPage.xaml.cs
+++ TokonatsuApp/TokonatsuApp/Views/MainPage.xaml.cs
@@ -2,17 +2,23 @@
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 using TokonatsuApp.ViewModels;
 
 namespace TokonatsuApp.Views
 {
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : Xamarin.Forms.TabbedPage
     {
+        
         public MainPage()
         {
+            On<Android>()
+                .SetToolbarPlacement(ToolbarPlacement.Bottom)
+                .SetBarItemColor(Color.FromHex("#FFB144"))
+                .SetBarSelectedItemColor(Color.ForestGreen);
             InitializeComponent();
-                       
         }
     }
 }
